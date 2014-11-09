16e715ad500bec576f8a524683ef56080033aba3
pablo <pablo@gmail.com>
2014-11-08 20:42:42 -0600

    Initial commit

---
+++ src/main/java/ac/cr/tec/ce1103/cedms/App.java
@@ -0,0 +1,13 @@
+package ac.cr.tec.ce1103.cedms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ac/cr/tec/ce1103/cedms/data/Xml.java
@@ -0,0 +1,7 @@
+package ac.cr.tec.ce1103.cedms.data;
+
+/**
+ * Created by pablo on 8/11/14.
+ */
+public class Xml {
+}
